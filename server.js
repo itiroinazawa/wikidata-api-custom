2f041f07d238a4683374cc74de8412b036368b43
Itiro Inazawa <itiro.inazawa@hotmail.com>
2017-12-19 00:32:11 -0200

    Atualização da aplicação e modificações no wikidata-search

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
-var WikidataSearch = require('wikidata-search').WikidataSearch;
-var wikidataSearch = new WikidataSearch()
-var util = require('util');
+var searcher = require("./structure/wikidata.js");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,77 +20,14 @@ router.get('/', function(req, res) {
 router.route('/search')
 	.post(function(req, res) {
 		
-		var field = req.body.field;			
-		searching(field, res, function(result, err){
-			
+		var field = req.body.field;		
+		
+		console.log('Searching term: ' + field)
+		
+		searcher.searching(field, res, function(result, err){
 			res.json(result);
-		});		
+		});
 	})
-
-function searching (field, res, cb){
-	
-	wikidataSearch.set('search', field);
-	
-	wikidataSearch.search(function(result, err) {
-
-		//Check for errors.
-		if (err) {
-			console.log('Uh oh, we got an error! : ' + err);
-			return;
-		}
-
-		console.log(util.inspect(result, true, null));
-
-		var list = []
-		
-		ListarEntidades(result.results, function(resultadoo, err3){			
-			return cb({resultadoo}, '');
-		});		 
-	});		 		
-};
-
-function ListarEntidades(entities, cb){
-	
-	var lista = [];
-	var processou = 0;
-	
-	entities.forEach(function(resulting, index, array){
-			
-		var entityId = resulting.id;
-		
-		
-		wikidataSearch.getEntities([entityId], true, function(result, err) {
-
-			if (err) {
-				console.log('Uh oh, we got an error! : ' + err);
-				return;
-			}
-			
-			console.log(util.inspect(result, true, null));				
-			
-			listarItems(result.entities[0], function(resultado, err2){
-				lista.push(resultado);			
-				processou++;
-			})				
-			
-			if(processou == array.length){				
-				return cb(lista, '');
-			}
-		});						
-	});
-	
-	
-}
-
-function listarItems(entity, cb){
-	
-	var list = []
-	entity.claims.forEach(function (item, index, array){
-		list.push({info: item.property + ': ' + item.value});
-	});		
-
-	return cb({'label' : entity.label, 'list' : list}, '');
-}
 
 app.use('/api', router);
 
---
+++ structure/wikidata.js
@@ -0,0 +1,85 @@
+var WikidataSearch = require('wikidata-search').WikidataSearch;
+var wikidataSearch = new WikidataSearch()
+var util = require('util');
+
+var exports = module.exports = {};
+
+exports.searching = function(field, res, cb){
+	
+	wikidataSearch.set('search', field);
+	
+	wikidataSearch.search(function(result, err) {
+
+		//Check for errors.
+		if (err) {
+			console.log('Uh oh, we got an error! : ' + err);
+			return;
+		}
+
+		//console.log(util.inspect(result, true, null));
+
+		var list = []
+		
+		console.log(util.inspect(result, true, null));		
+		
+		if(result.results.length > 0)
+		{
+			ListarEntidades(result.results, function(result, err3){			
+				return cb({result}, '');
+			});		 
+		}
+		else{
+			return cb({message : 'No results found'}, '');
+		}
+	});		 		
+};
+
+function ListarEntidades(entities, cb){
+	
+	var lista = [];
+	var processou = 0;
+	
+	entities.forEach(function(result, index, array){
+			
+		var entityId = result.id;
+		
+		
+		wikidataSearch.getEntities([entityId], true, function(result, err) {
+
+			if (err) {
+				console.log('Uh oh, we got an error! : ' + err);
+				return;
+			}
+			
+			//console.log(util.inspect(result, true, null))
+			
+			listarItems(result.entities[0], function(result, err2){
+				lista.push(result);			
+				processou++;
+			})				
+			
+			if(processou == array.length){				
+				return cb(lista, '');
+			}
+		});						
+	});
+	
+	
+}
+
+function listarItems(entity, cb){
+	
+	var list = [];
+
+	if(entity != undefined){		
+		
+		entity.claims.forEach(function (item, index, array){
+			list.push({info: item.property + ': ' + item.value});
+		});		
+		
+		return cb({'label' : entity.label, 'list' : list}, '');
+	}
+	else{
+		return cb({}, '');
+	}
+}
